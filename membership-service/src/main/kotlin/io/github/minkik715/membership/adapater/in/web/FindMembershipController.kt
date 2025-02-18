90dc65bdefab3fb6d91992c58377cc60fd4b757b
minki <minkik715@gmail.com>
2025-02-18 20:05:30 +0900

    feat: test 추가

--- membership-service/src/main/kotlin/io/github/minkik715/membership/adapater/in/web/FindMembershipController.kt
+++ membership-service/src/main/kotlin/io/github/minkik715/membership/adapater/in/web/FindMembershipController.kt
@@ -3,9 +3,7 @@ package io.github.minkik715.membership.adapater.`in`.web
 import io.github.minkik715.common.WebAdapter
 import io.github.minkik715.membership.application.port.`in`.FindMembershipQuery
 import io.github.minkik715.membership.application.port.`in`.FindMembershipUseCase
-import io.github.minkik715.membership.application.port.`in`.RegisterMembershipUseCase
 import io.github.minkik715.membership.domain.Membership
-import io.github.minkik715.membership.domain.MembershipId
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,7 +16,7 @@ class FindMembershipController(
 ) {
 
     @GetMapping("/memberships/{membershipId}")
-    fun getMembershipByMemeberId(@PathVariable("membershipId") membershipId: Long): ResponseEntity<Membership> {
+    fun getMembershipByMemberId(@PathVariable("membershipId") membershipId: Long): ResponseEntity<Membership> {
         return ResponseEntity.ok(findMembershipUseCase.getMembershipByMembershipId(FindMembershipQuery(membershipId)))
     }
 }
